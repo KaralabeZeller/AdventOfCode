700cf59a33122c095bb7e234f6fe0eb7813aea41
KaralabeZeller <g.fertetics@hotmail.com>
2023-12-06 12:27:44 +0100

    Update day6.py

--- day6.py
+++ day6.py
@@ -1,3 +1,6 @@
+import math
+
+# Bruteforce solution
 with open('input.txt', 'r') as file:
     lines = file.readlines()
     time = int(''.join(lines[0].split(':')[1].split()))
@@ -6,5 +9,13 @@ with open('input.txt', 'r') as file:
 sum_bests = sum(1 for second in range(1, time // 2) if second * (time - second) > distance)
 
 final_sum = sum_bests * 2 + 1 if time % 2 == 0 else (sum_bests + 1) * 2
+
+print("SUM:", final_sum)
+
+# 'math' solution
+x1 = (-time + math.sqrt(time ** 2 - 4 * (-1) * -distance)) / (2 * (-1))
+x2 = (-time - math.sqrt(time ** 2 - 4 * (-1) * -distance)) / (2 * (-1))
+
+final_sum = math.ceil(x2) - math.ceil(x1)
 
 print("SUM:", final_sum)
