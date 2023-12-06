05a073dc329276923d7879bd2cf7beab11106a21
KaralabeZeller <g.fertetics@hotmail.com>
2023-12-06 12:09:07 +0100

    Update day6.py

--- day6.py
+++ day6.py
@@ -1,15 +1,10 @@
 with open('input.txt', 'r') as file:
     lines = file.readlines()
-    time_list = [int(''.join(lines[0].split(':')[1].split()))]
-    distance_list = [int(''.join(lines[1].split(':')[1].split()))]
-    
-sum_bests = 0
-time = time_list[0]
-distance = distance_list[0]
+    time = int(''.join(lines[0].split(':')[1].split()))
+    distance = int(''.join(lines[1].split(':')[1].split()))
 
-for second in range(1, time):
-    theoretical_best = second * (time - second)
-    if(theoretical_best > distance):
-        sum_bests += 1
+sum_bests = sum(1 for second in range(1, time // 2) if second * (time - second) > distance)
 
-print("SUM: ", sum_bests)
+final_sum = sum_bests * 2 + 1 if time % 2 == 0 else (sum_bests + 1) * 2
+
+print("SUM:", final_sum)
